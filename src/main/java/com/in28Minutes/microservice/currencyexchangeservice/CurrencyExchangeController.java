a900761b9e41e510dac0a48fd2d24f210a06b40a
Divya Gupta <divygupta0319@gmail.com>
2023-03-21 14:07:02 +0530

    Migrated imports from javax to jakarta, added logging to controller, and defined logging pattern to log trace id and span id, in properties file

--- src/main/java/com/in28Minutes/microservice/currencyexchangeservice/CurrencyExchangeController.java
+++ src/main/java/com/in28Minutes/microservice/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,5 +1,7 @@
 package com.in28Minutes.microservice.currencyexchangeservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CurrencyExchangeController {
+	
+	private Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
 	
 	@Autowired
 	private CurrencyExchangeRepository repository;
@@ -18,6 +22,8 @@ public class CurrencyExchangeController {
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
 	public CurrencyExchange retrieveExchangeValue(
 			@PathVariable String from, @PathVariable String to) {
+		
+		logger.info("retrieveExchangeValue called with {} to {}", from, to);
 		
 		CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to);
 		
